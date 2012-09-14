35f100703329dc70d10d2741e8d6e0267ada2345
DigTheDoug <digthedoug@hotmail.com>
2012-09-13 23:56:15 -0400

    initial commit of code and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# SyllableCounter
+
+A fairly light and simple Python syllable counter. Created mainly for use with proper names and words that could not be found in a dictionary. It's farily accurate and _can_ work for common defined words by simply passing in an argument, allthough it's accuracy is only very lightly tested.
+
+Based on/modified from [Joe Basirico's JS code provided on StackOverflow](http://stackoverflow.com/a/5615724/188221)
---
+++ SyllableCounter.py
@@ -0,0 +1,41 @@
+# Version 1.0
+# 2012 Doug Hogan
+# https://github.com/DigTheDoug
+
+# Based on/modified from Joe Basirico's JS code here:
+# http://stackoverflow.com/a/5615724/188221
+def CountSyllables(word, isName=True):
+    vowels = "aeiouy"
+    specials = ["ia"]  #split as separate syllables
+    if isName:
+        #single syllables in words like bread and lead, but split in names like Breanne and Adreann
+        specials.extend("ea")
+    specials_except_end = ["ie","ya","es","ed"]  #seperate syllables unless ending the word
+    currentWord = word.lower()
+    numVowels = 0
+    lastWasVowel = False
+    last_letter = ""
+
+    for letter in currentWord:
+        if letter in vowels:
+            #don't count diphthongs unless special cases
+            combo = last_letter+letter
+            if lastWasVowel and combo not in specials and combo not in specials_except_end:
+                lastWasVowel = True
+            else:
+                numVowels += 1
+                lastWasVowel = True
+        else:
+            lastWasVowel = False
+
+        last_letter = letter
+
+    #remove es & ed which are usually silent
+    if len(currentWord) > 2 and currentWord[-2:] in specials_except_end:
+        numVowels -= 1
+
+    #remove silent single e, but not ee since it counted it before and we should be correct
+    elif len(currentWord) > 2 and currentWord[-1:] == "e" and currentWord[-2:] != "ee":
+        numVowels -= 1
+
+    return numVowels
